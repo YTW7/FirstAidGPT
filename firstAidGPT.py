037c4e5bda3ff22621b4d4525ebcdada91cd865e
Mohd Yahya <91966435+YTW7@users.noreply.github.com>
2023-02-13 05:49:56 +0530

    Add files via upload

---
+++ firstAidGPT.py
@@ -0,0 +1,43 @@
+from langchain.llms import OpenAI
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.docstore.document import Document
+import requests
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores.faiss import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+
+
+
+def get_wiki_data(title, first_paragraph_only):
+    url = f"https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=1&titles={title}"
+    if first_paragraph_only:
+        url += "&exintro=1"
+    data = requests.get(url).json()
+    return Document(
+        page_content=list(data["query"]["pages"].values())[0]["extract"],
+        metadata={"source": f"https://en.wikipedia.org/wiki/{title}"},
+    )
+
+sources = [
+     get_wiki_data("Headache", True),
+    get_wiki_data("Myocardial_infarction", True),
+
+]
+
+
+chain = load_qa_with_sources_chain(OpenAI(temperature=0))
+
+def print_answer(question):
+    print(
+        chain(
+            {
+                "input_documents": sources,
+                "question": question,
+            },
+            return_only_outputs=True,
+        )["output_text"]
+    )
+
+
+
+
